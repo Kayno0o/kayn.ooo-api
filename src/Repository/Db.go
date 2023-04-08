96254536fa3a2474ef4de77f48ebf1bd1b4df04c
Kayno0o <kevyn.fyleyssant@gmail.com>
2023-04-09 01:45:13 +0200

    feature: add dsn to .env

--- src/Repository/Db.go
+++ src/Repository/Db.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	entity "kayn.ooo/api/src/Entity"
@@ -9,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "root:password@tcp(localhost)/kayn.ooo?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DB_DSN")
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
