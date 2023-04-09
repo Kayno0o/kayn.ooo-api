74a9f9d6d81906ff9bb7430b42c930439e1fd2d3
Kayno0o <kevyn.fyleyssant@gmail.com>
2023-04-09 03:00:02 +0200

    fix: avoid error duplication

--- src/Middleware/WriteJSON.go
+++ src/Middleware/WriteJSON.go
@@ -6,7 +6,9 @@ import (
 )
 
 func WriteJSON(w http.ResponseWriter, data interface{}, status int) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+	if w.Header().Get("Content-Type") == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+		json.NewEncoder(w).Encode(data)
+	}
 }
